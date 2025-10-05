e63d590fed7a4ed446740f8a451a7141bfa78778
Alagar <alagar17302@gmail.com>
2025-10-05 16:16:47 +0530

    added timeout

--- src/config/api.js
+++ src/config/api.js
@@ -7,7 +7,7 @@ const API_BASE_URL = import.meta.env.VITE_APP_API_BASE_URL;
 
 export const apiClient = axios.create({
   baseURL: API_BASE_URL,
-  timeout: 15000,
+  timeout: 30000,
   headers: {
     'Content-Type': 'application/json',
   },
