a95136c532ae6e361d0ecebc1a3326de72042120
Alagar <alagar17302@gmail.com>
2025-10-28 11:16:24 +0530

    added scroll top

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -8,38 +8,34 @@ import CustomOrderButton from '../components/common/CustomOrderButton'
 import CustomOrderButtonMobile from '../components/common/CustomOrderButtonMobile'
 import { useMediaQuery } from '../hooks/useMediaQuery'
 
+import ScrollToTop from '../components/common/ScrollToTop';
+
 const MainLayout = () => {
   const contentRef = useContentLoader();
   const location = useLocation();
   const isMobile = useMediaQuery('(max-width: 768px)');
 
-  // Define routes where footer should be hidden
   const hideFooterRoutes = ['/login', '/register', '/forgot-password', '/reset-password'];
-  const shouldHideFooter = hideFooterRoutes.some(route => 
-    location.pathname.includes(route)
-  );
+  const shouldHideFooter = hideFooterRoutes.some(route => location.pathname.includes(route));
 
-  // Define routes where custom order button should be hidden
   const hideCustomButtonRoutes = ['/admin', '/login', '/register', '/forgot-password', '/reset-password'];
-  const shouldHideCustomButton = hideCustomButtonRoutes.some(route => 
-    location.pathname.includes(route)
-  );
+  const shouldHideCustomButton = hideCustomButtonRoutes.some(route => location.pathname.includes(route));
 
   return (
     <div className='min-h-screen flex flex-col bg-white dark:bg-gray-900 smokey:bg-gray-800 transition-colors duration-300'>
-       <AppHeader />
-       <main ref={contentRef} className='flex-1'>
+      <AppHeader />
+      <ScrollToTop /> {/* 👈 Add this line */}
+      <main ref={contentRef} className='flex-1'>
         <Outlet />
-       </main>
-       
-       {/* Custom Order Button - Show only on user pages */}
-       {!shouldHideCustomButton && (
-         isMobile ? <CustomOrderButtonMobile /> : <CustomOrderButton />
-       )}
-       
-       {!shouldHideFooter && <Footer />}
+      </main>
+
+      {!shouldHideCustomButton && (
+        isMobile ? <CustomOrderButtonMobile /> : <CustomOrderButton />
+      )}
+      {!shouldHideFooter && <Footer />}
     </div>
-  )
-}
+  );
+};
+
 
 export default MainLayout;
